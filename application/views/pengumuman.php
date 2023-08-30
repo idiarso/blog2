72d3159ea073b191a059430d853addb35d9ed80a
Mas idi <idiarsosimbang@gmail.com>
2023-08-30 14:30:11 +0700

    baru1

--- application/views/pengumuman.php
+++ application/views/pengumuman.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title><?= strtoupper($this->db->get("profile")->row()->nama); ?> | OFFICIAL</title>
+    <title><?= strtoupper($this->db->get("profile")->row()->nama); ?> | ORGANISASI</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
